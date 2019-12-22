41d8e0b4149e6c1775724f4a1de2b1c681cec956
ksb0511 <soobin796@gmail.com>
2019-12-23 05:30:50 +0900

    첫 세팅

--- app/src/androidTest/java/com/project/mobo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/project/mobo/ExampleInstrumentedTest.kt
@@ -12,7 +12,7 @@ import org.junit.Assert.*
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
- */
+ *///
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
