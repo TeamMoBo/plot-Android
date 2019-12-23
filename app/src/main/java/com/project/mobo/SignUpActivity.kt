d60dc6355f4ee24c2423f0ea743a961a76abd916
ksb0511 <soobin796@gmail.com>
2019-12-23 17:16:26 +0900

    첫 세팅

--- app/src/main/java/com/project/mobo/SignUpActivity.kt
+++ app/src/main/java/com/project/mobo/SignUpActivity.kt
@@ -8,5 +8,6 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        //
     }
 }
