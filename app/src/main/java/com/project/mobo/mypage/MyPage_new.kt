5666a89e0cacf799fd6226e08afcec34b5cae03f
G-hyeon <56578913+G-hyeon@users.noreply.github.com>
2019-12-24 14:42:24 +0900

    Add Mypage

---
+++ app/src/main/java/com/project/mobo/mypage/MyPage_new.kt
@@ -0,0 +1,13 @@
+package com.project.mobo.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.mobo.R
+
+class MyPage_new : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page_new)
+    }
+}
