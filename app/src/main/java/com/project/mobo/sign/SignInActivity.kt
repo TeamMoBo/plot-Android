225aa954fa647789470b152817a07a018bc1d091
ksb0511 <soobin796@gmail.com>
2019-12-29 05:08:52 +0900

    response_body fail

--- app/src/main/java/com/project/mobo/sign/SignInActivity.kt
+++ app/src/main/java/com/project/mobo/sign/SignInActivity.kt
@@ -22,10 +22,9 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-
-
-        //initialUI()
-        login()
+        
+        initialUI()
+        //login()
 
     }
     private fun initialUI(){
