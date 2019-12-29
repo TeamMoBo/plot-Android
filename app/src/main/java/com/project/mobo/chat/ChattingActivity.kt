53c3bf0ba74b14d680cc8727dbb52f4202f7c08a
jihye0420 <gh06095@naver.com>
2019-12-29 17:44:44 +0900

    firebase 채팅 서버 로직 구현 - 메시지 보내기 - 메시지 받아오기 ->화면 그리기 까지 완료(채팅 뷰 작업은 아직 미완성)

--- app/src/main/java/com/project/mobo/chat/ChattingActivity.kt
+++ app/src/main/java/com/project/mobo/chat/ChattingActivity.kt
@@ -3,6 +3,7 @@ package com.project.mobo.chat
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.database.*
 import com.project.mobo.R
 import com.project.mobo.common.FirebaseChatConstant
@@ -14,8 +15,17 @@ class ChattingActivity : AppCompatActivity() {
     private val databaseReference: DatabaseReference = FirebaseDatabase.getInstance().reference
     private lateinit var chatRoomId: String
     private lateinit var chatRoom: DatabaseReference
+    private lateinit var chatUsers: DatabaseReference
+
+    private var trueMap = HashMap<String, Boolean>()
+    private var falseMap = HashMap<String, Boolean>()
+
+    private lateinit var lm: LinearLayoutManager
+    private lateinit var adapter: ChatRoomAdapter
+
+
     //TODO: 실제로는 로그인 시점에 받아온 firebase database uid 를 받아서 세팅해야함
-    private val uid: String = "seungmin"
+    private val uid: String = "jihye"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +41,17 @@ class ChattingActivity : AppCompatActivity() {
             .child(chatRoomId)
             .child(FirebaseChatConstant.COMMENTS)
 
+        chatUsers = databaseReference
+            .child(FirebaseChatConstant.CHATROOMS)
+            .child(chatRoomId)
+            .child("users")
+
+        trueMap[uid] = true
+        falseMap[uid] = false
+        chatUsers.push().setValue(trueMap)
+
+        //초기화
+        init()
         //전송 버튼 클릭시 메세지를 파이어베이스 Realtime DataBase 에 쓰는 코드
         setupSendButton()
         //메시지를 보여주기 위한 리스트 어댑터 생성 및 세팅
@@ -38,6 +59,29 @@ class ChattingActivity : AppCompatActivity() {
         //데이터 읽어오는 것
         setupChatRoomUpdated()
     }
+
+    private fun init() {
+        lm = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+        adapter = ChatRoomAdapter()
+        rvChatList.layoutManager = lm
+        rvChatList.adapter = adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        chatUsers.push().setValue(falseMap)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        chatUsers.push().setValue(falseMap)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        chatUsers.push().setValue(trueMap)
+    }
+
 
     private fun setupSendButton() {
         btnSend.setOnClickListener {
@@ -61,9 +105,10 @@ class ChattingActivity : AppCompatActivity() {
         chatRoom
             .addChildEventListener(
                 onChildAdded = { snap, _ ->
-                    snap.getValue(ChatData::class.java)?.let {
-                        Log.d("siba", "firebase data siba : $it")
-                    } ?: Log.e("siba", "firebase go to hell fuck")
+                    adapter.addItem(snap.getValue(ChatData::class.java)!!)
+//                    snap.getValue(ChatData::class.java)?.let {
+//                        Log.d("siba", "firebase data siba : $it")
+//                    } ?: Log.e("siba", "firebase go to hell fuck")
                 }
             )
     }
