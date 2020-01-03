c1b1c76b71473e6e33774f5883bf9dba1eb3fa16
min_jjiny <tm003512@gmail.com>
2020-01-04 06:01:48 +0900

    [민진]마이페이지 보유 티켓 서버 통신

--- app/src/main/java/com/project/mobo/mypage/MyPage_new.kt
+++ app/src/main/java/com/project/mobo/mypage/MyPage_new.kt
@@ -21,7 +21,9 @@ import com.project.mobo.api.*
 import kotlinx.android.synthetic.main.activity_my_page_new.*
 
 class MyPage_new : AppCompatActivity() {
-//
+    //
+    lateinit var myTicketData: HaveTicketData
+
     @SuppressLint("ObsoleteSdkInt")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +35,7 @@ class MyPage_new : AppCompatActivity() {
 
         var mypageInfo: MyPageDataD
         callMypage.safeEnqueue {
-            if (it.isSuccessful){
+            if (it.isSuccessful) {
                 mypageInfo = it.body()!!.data
                 profiletxt.setText(mypageInfo.userComment)
                 profile_name.setText(mypageInfo.userNickname)
@@ -43,7 +45,7 @@ class MyPage_new : AppCompatActivity() {
                 edittxt_livingarea.setText(mypageInfo.userLocation)
                 edittxt_name.setText(mypageInfo.userName)
                 edittxt_id.setText(mypageInfo.userId)
-                when(mypageInfo.userSelectGender){
+                when (mypageInfo.userSelectGender) {
                     0 -> rg_btn2_mypage.isChecked = true
                     1 -> rg_btn1_mypage.isChecked = true
                     2 -> rg_btn3_mypage.isChecked = true
@@ -54,12 +56,25 @@ class MyPage_new : AppCompatActivity() {
             }
         }
 
+        val callMyHaveServiece = UserServiceImpl.haveTicketService.responseHaveTicket(
+            key = SharedPreferenceController.getUserToken(this)
+        )
+
+        callMyHaveServiece.safeEnqueue {
+            if(it.isSuccessful){
+                myTicketData = it.body()!!.data
+                txt_ticket.text = myTicketData.userTicket.toString()
+                txt_popcorn.text = myTicketData.userPopcorn.toString()
+            }
+        }
+
 
         //Change profile Image
         profile_image.setOnClickListener {
             if (VERSION.SDK_INT >= VERSION_CODES.M) {
                 if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
-                    PackageManager.PERMISSION_DENIED) {
+                    PackageManager.PERMISSION_DENIED
+                ) {
 
                     val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
                     requestPermissions(permissions, PERMISSION_CODE)
@@ -91,21 +106,24 @@ class MyPage_new : AppCompatActivity() {
             editText(profile_introOneLine)
         }
 
-        val listEditText: ArrayList<EditText> = arrayListOf<EditText>(edittxt_age, edittxt_school,
+        val listEditText: ArrayList<EditText> = arrayListOf<EditText>(
+            edittxt_age, edittxt_school,
             edittxt_department, edittxt_livingarea, edittxt_name, edittxt_id,
-            edittxt_kakaotalk, edittxt_ageMin, edittxt_ageMax)
+            edittxt_kakaotalk, edittxt_ageMin, edittxt_ageMax
+        )
 
         //val listRadioButton: ArrayList<RadioButton> = arrayListOf(rg_btn1, rg_btn2, rg_btn3)
-        val listRadioButton: ArrayList<RadioButton> = arrayListOf(rg_btn1_mypage, rg_btn2_mypage, rg_btn3_mypage)
+        val listRadioButton: ArrayList<RadioButton> =
+            arrayListOf(rg_btn1_mypage, rg_btn2_mypage, rg_btn3_mypage)
 
         btn_change_info.setOnClickListener {
             btn_change_info.visibility = EditText.INVISIBLE
             btn_change_complete.visibility = EditText.VISIBLE
-            for (i in listEditText){
+            for (i in listEditText) {
                 editText(i)
             }
 
-            for (i in listRadioButton){
+            for (i in listRadioButton) {
                 editRadio(i)
             }
 
@@ -115,25 +133,24 @@ class MyPage_new : AppCompatActivity() {
         btn_change_complete.setOnClickListener {
             btn_change_info.visibility = EditText.VISIBLE
             btn_change_complete.visibility = EditText.INVISIBLE
-            for (i in listEditText){
+            for (i in listEditText) {
                 editText(i)
             }
 
-            for (i in listRadioButton){
+            for (i in listRadioButton) {
                 editRadio(i)
             }
         }
     }
 
-    private fun editText(eText: EditText){
-        if (eText.isFocusableInTouchMode){
+    private fun editText(eText: EditText) {
+        if (eText.isFocusableInTouchMode) {
             eText.isFocusableInTouchMode = false
             eText.isEnabled = false
-            if(eText.text.toString() == ""){
+            if (eText.text.toString() == "") {
                 eText.setText(eText.hint.toString())
             }
-        }
-        else if(!eText.isFocusableInTouchMode){
+        } else if (!eText.isFocusableInTouchMode) {
             eText.isEnabled = true
             eText.isFocusableInTouchMode = true
             eText.hint = eText.text
@@ -141,25 +158,24 @@ class MyPage_new : AppCompatActivity() {
         }
     }
 
-    private fun editRadio(eText: RadioButton){
-        if (eText.isFocusableInTouchMode){
+    private fun editRadio(eText: RadioButton) {
+        if (eText.isFocusableInTouchMode) {
             eText.isFocusableInTouchMode = false
             eText.isEnabled = false
-        }
-        else if(!eText.isFocusableInTouchMode){
+        } else if (!eText.isFocusableInTouchMode) {
             eText.isEnabled = true
             eText.isFocusableInTouchMode = true
         }
     }
 
     //Change profile image
-    private fun pickImageFromGallery(){
+    private fun pickImageFromGallery() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
-    companion object{
+    companion object {
         private val IMAGE_PICK_CODE = 1000
         private val PERMISSION_CODE = 1001
     }
@@ -169,13 +185,13 @@ class MyPage_new : AppCompatActivity() {
         permissions: Array<out String>,
         grantResults: IntArray
     ) {
-        when(requestCode){
+        when (requestCode) {
             PERMISSION_CODE -> {
                 if (grantResults.size > 0 && grantResults[0] ==
-                    PackageManager.PERMISSION_GRANTED){
+                    PackageManager.PERMISSION_GRANTED
+                ) {
                     pickImageFromGallery()
-                }
-                else{
+                } else {
                     Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
                 }
             }
@@ -184,12 +200,10 @@ class MyPage_new : AppCompatActivity() {
 
     @SuppressLint("MissingSuperCall")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE) {
             profile_image.setImageURI(data?.data)
-    }
-
-        else if (requestCode == 1){
-            if(resultCode == Activity.RESULT_OK) run {
+        } else if (requestCode == 1) {
+            if (resultCode == Activity.RESULT_OK) run {
                 val newName: String? = data?.getStringExtra("data")
                 profile_name.setText(newName)
             }
